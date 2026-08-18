e1abbe5b8d94fe7151bbc136ffe773f5e58bbef9
ShawnRn <shawnrain.me@gmail.com>
2026-08-18 10:06:05 +0800

    fix(winui): 增加 Program.cs 引导与标准 ICO，完善 Unpackaged 构建配置

---
+++ src/MiAir.WinUI/Program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Threading;
+using Microsoft.UI.Dispatching;
+using Microsoft.UI.Xaml;
+
+namespace MiAir.WinUI;
+
+public static class Program
+{
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        WinRT.ComWrappersSupport.InitializeComWrappers();
+        Application.Start((p) =>
+        {
+            var context = new DispatcherQueueSynchronizationContext(DispatcherQueue.GetForCurrentThread());
+            SynchronizationContext.SetSynchronizationContext(context);
+            _ = new App();
+        });
+    }
+}
